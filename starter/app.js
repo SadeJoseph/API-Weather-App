ca4c79dffdfd0f99d362edd6d7394d2f08c74c98
SadeJoseph <feojoseph1@gmail.com>
2023-02-24 19:43:00 +0000

    some variables and stylig

---
+++ starter/app.js
@@ -0,0 +1,27 @@
+//global
+
+var apiKey = 'd32aec8396112b0fae9348457dab7aa2';
+var searchBtn = $('#search-button');
+var weatherS = $('.weather-search');
+var todayS = $('#today');
+var forecastS = $('#forecast');
+var storedHist= $('#history');
+
+//display 5 day forecast from city 
+
+//title
+
+//results-loop
+
+//moment js
+
+//push results to html page 
+
+
+//display weather and display new search . make alert for invalid city 
+
+
+//local storage- new city searches aand display curernt weather
+
+
+
