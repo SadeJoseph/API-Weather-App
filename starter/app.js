e35662440d829fab1b9d8a5cb0690db6b39f88af
SadeJoseph <feojoseph1@gmail.com>
2023-02-24 19:45:01 +0000

    html

--- starter/app.js
+++ starter/app.js
@@ -8,15 +8,34 @@ var forecastS = $('#forecast');
 var storedHist= $('#history');
 
 //display 5 day forecast from city 
-
+function forecastDis(results) {
 //title
-
+$(`<h4 class="mt-1 font-weight-bold">5-Day Forecast:</h4>`).insertAfter(todayS);
 //results-loop
-
+for (var day of results.list) {
 //moment js
-
+var currentDay = moment.unix(day.dt).format("DD/MM/YYYY");
+var currentHour= moment.unix(day.dt).format("hh:mm:ss a");
 //push results to html page 
+    if (currentHour == '12:00:00 pm') {
+        forecastS.append(`
+            
+        <div class="mr-3 ml-3 pr-5 pl-2 pt-2 border border-dark forecast-card">
+        <h6 class='font-weight-bold'>${currentDay}</h6>
+        <h6>
+        <p><img src="https://openweathermap.org/img/w/${day.weather[0].icon}.png" alt="weather icon"></p>
+        <p>Temp: ${Math.round(day.main.temp)} C°</p>
+        <p>Wind: ${day.wind.speed} KPH</p>
+        <p>Humidity: ${day.main.humidity} %</p>
+        </h6>
+        </div>
+        `)
+}
 
+}
+
+
+}
 
 //display weather and display new search . make alert for invalid city 
 
